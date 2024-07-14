daf962f6793a1b3354d463de020979e70efd9c65
Rohit kumar <rohit.kumar@hashicorp.com>
2024-07-14 11:00:38 +0530

    Adding Support to Import AKS Resources

---
+++ import_aks.py
@@ -0,0 +1,119 @@
+from utils.utilities import Utilities, SkipTag
+from jinja2 import Environment, FileSystemLoader
+from loguru import logger
+import os
+from utils.cleanup import cleanup_tf_plan_file
+import sys
+
+
+class AKSImportSetUp:
+    """
+    Import Block for AKS Import.
+    Supoprted resources: AKS, Addons, NodePools, ScaleSet
+    """
+
+    def __init__(self, subscription_id, region, resource, local_repo_path, filters):
+        self.aks_client = Utilities.create_client(subscription_id = subscription_id, resource=resource,)
+        self.resource_client = Utilities.create_client(subscription_id, resource="resource_group")
+        self.tmpl = Environment(loader=FileSystemLoader("templates"))
+        self.region = region        
+        self.local_repo_path = local_repo_path
+        self.subscription_id = subscription_id
+        self.tag_filters = {key: value for key, value in filters} if filters else {}
+
+
+
+    def _tags_match(self, resource_tags):
+            """
+            Check if resource tags match the filters.
+            """
+            if resource_tags.get('TF_IMPORTED') == 'True':
+                return False
+            
+            for key, value in self.tag_filters.items():
+                if key not in resource_tags or resource_tags[key] != value:
+                    return False
+            return True
+
+    def describe_aks_cluster(self):
+        """
+        Get Cluster details for all AKS clusters in the subscription
+        """
+        cluster_details = []
+
+        resource_groups = self.resource_client.resource_groups.list()
+
+        for rg in resource_groups:
+            clusters = self.aks_client.managed_clusters.list_by_resource_group(rg.name)
+
+            for cluster in clusters:
+                #Check if the cluster matches the tag filters
+                cluster_tags = cluster.tags or {}
+                if not self._tags_match(cluster_tags):
+                    continue
+                
+                cluster_detail = self.aks_client.managed_clusters.get(rg.name, cluster.name)
+                
+                node_pools =[]
+                for pool in cluster_detail.agent_pool_profiles:
+                    
+                    if pool.mode == "System": # Skip nodepool if mode of nodepool is "System"
+                        continue
+                    node_pools.append(pool.name)
+
+                cluster_info = {
+                    'cluster_name': cluster_detail.name,
+                    'location': cluster_detail.location,
+                    'dns_prefix': cluster_detail.dns_prefix,
+                    'node_pools': node_pools,
+                    'node_resource_group': cluster_detail.node_resource_group,
+                    "resource_group": rg.name
+                }
+                cluster_details.append(cluster_info)
+        logger.info(f"Total AKS Cluster Found: { len(cluster_details) }")
+        
+        return cluster_details
+
+    def generate_import_blocks(self, aks_cluster_details):
+        """
+        Generate Import Blocks, Generate Terraform code, Cleanup Terraform code
+        """
+        if not aks_cluster_details:
+            logger.info("No AKS Cluster found: Nothing to do. Exitting")
+            sys.exit(1)
+
+        template = self.tmpl.get_template("aks_import.tf.j2")
+
+        for aks_cluster in aks_cluster_details:
+            logger.info(f"Importing : {aks_cluster}")
+
+            context = {"cluster_name": aks_cluster["cluster_name"], "subscription_id": self.subscription_id, "resource_group": aks_cluster['resource_group'], "node_pools": aks_cluster['node_pools'] }
+
+            rendered_template = template.render(context)
+
+            output_file_path = f"{self.local_repo_path}/import-{aks_cluster['cluster_name']}.tf"
+            with open(output_file_path, "w") as f:
+                f.write(rendered_template)
+
+            Utilities.run_terraform_cmd(["terraform", f"-chdir={self.local_repo_path}", "plan", f"-generate-config-out=generated-plan-import-{aks_cluster['cluster_name']}.tf"])
+            os.rename(output_file_path, f"{output_file_path}.imported")
+            cleanup_tf_plan_file(input_tf_file=f"{self.local_repo_path}/generated-plan-import-{aks_cluster['cluster_name']}.tf")
+            exit(1)
+        for filename in os.listdir(self.local_repo_path):
+            if filename.endswith(".imported"):
+                new_filename = filename.replace(".imported", "")
+                old_file = os.path.join(self.local_repo_path, filename)
+                new_file = os.path.join(self.local_repo_path, new_filename)
+                os.rename(old_file, new_file)
+
+    def set_everything(self):
+        """
+        Setup the WorkFlow Steps.
+        """
+        Utilities.generate_tf_provider(self.local_repo_path, region=self.region)
+        Utilities.run_terraform_cmd(["terraform", f"-chdir={self.local_repo_path}", "init"])
+
+        aks_clusters = self.describe_aks_cluster()
+        self.generate_import_blocks(aks_clusters)
+        Utilities.run_terraform_cmd(["terraform", f"-chdir={self.local_repo_path}", "fmt"])
+        Utilities.run_terraform_cmd(["terraform", f"-chdir={self.local_repo_path}", "plan"])
---
+++ utils/cleanup.py
@@ -0,0 +1,140 @@
+import re
+import sys
+from loguru import logger
+import os
+import subprocess
+
+# Define the RESOURCE_CLEANUP dictionary with patterns properly escaped
+RESOURCE_CLEANUP = {
+    "global": ["null"],
+    "multiline_pattern": [
+        r"os_profile\s*\{\s*\n\s*\}",
+    ],
+    "azurerm_virtual_machine": ["= 0", "os_profile"],
+    "azurerm_kubernetes_cluster": [
+        "= 0",
+        "= \[\]",
+    ],
+    "azurerm_kubernetes_cluster_node_pool": [
+        "= 0",
+        "= \[\]",
+    ],
+}
+
+
+def remove_global_lines(tf_file, list_to_cleanup):
+    output_file = tf_file  # f"{tf_file}-global-cleanup.tf"
+    logger.info(f"Removing lines containing following items: {list_to_cleanup}")
+
+    with open(tf_file, "r") as readfile:
+        lines = readfile.readlines()
+        filtered_lines = []
+
+        for line in lines:
+            if any(element in line for element in list_to_cleanup):
+                # If 'Condition' is in the line and '= {}' is one of the elements, do not remove it.
+                # Special rule for aws_iam_role properties assume_role_policy of jsonencode block.
+                if "= {}" in line and "Condition" in line:
+                    filtered_lines.append(line)
+                continue
+            filtered_lines.append(line)
+
+    with open(output_file, "w") as write_file:
+        write_file.writelines(filtered_lines)
+
+    logger.info(f"Generated intermediate file to process: {output_file}")
+    return output_file
+
+
+def remove_multiline(file, patterns):
+    with open(file, "r") as readfile:
+        content = readfile.read()
+
+        for pattern in patterns:
+            matches = re.findall(pattern, content, re.MULTILINE | re.DOTALL)
+            # Remove the matches from the content
+            content = re.sub(pattern, "", content, flags=re.MULTILINE | re.DOTALL)
+
+    with open(file, "w") as writefile:
+        writefile.write(content)
+
+
+def should_remove_line(line, resource_type, custom_pattern=[]):
+    """
+    Determine if a line should be removed based on the resource type and global patterns.
+    """
+    if not custom_pattern:
+        patterns = RESOURCE_CLEANUP.get(resource_type, [])
+    else:
+        patterns = custom_pattern
+
+    for pattern in patterns:
+        if "min_size" in line and resource_type in ("aws_autoscaling_group", "aws_eks_node_group"):  # for EKS Cluster aws_autoscaling_group, aws_eks_node_group
+            return False
+        if re.search(pattern, line):
+            return True
+    return False
+
+
+def process_terraform_plan(input_file):
+    with open(input_file, "r") as file:
+        lines = file.readlines()
+
+    new_lines = []
+    in_resource_block = False
+    current_resource_type = None
+
+    # Special case ebs_volume Cleanup, Removing iops when type is gp2
+    is_iops_set = False
+    is_gp2_set = False
+
+    for line in lines:
+        # Check if the line starts a new resource block
+        resource_block_match = re.match(r'\s*resource\s+"(\w+)"\s+"[^"]+"\s+{', line)
+        if resource_block_match:
+            in_resource_block = True
+            current_resource_type = resource_block_match.group(1)
+
+            new_lines.append(line)
+            continue
+
+        # Check if the line ends a resource block
+        if in_resource_block and re.match(r"\s*}$\n#", line):
+            in_resource_block = False
+            current_resource_type = None
+            new_lines.append(line)
+            continue
+
+        # Process lines within a resource block
+        if in_resource_block and current_resource_type:
+            if current_resource_type == "aws_ebs_volume":  # EDGE case for removing iops when type is gp2
+                if "iops" in line:
+                    is_iops_set = True
+                    get_iops_line = line
+                if "gp2" in line:
+                    is_gp2_set = True
+                if is_gp2_set and is_iops_set:
+                    new_lines.remove(get_iops_line)
+                    is_iops_set = False
+                    is_gp2_set = False
+
+            if should_remove_line(line, current_resource_type):
+                continue
+        new_lines.append(line)
+
+    # Write the cleaned content to a new file
+    with open(input_file, "w") as new_file:
+        new_file.writelines(new_lines)
+
+    # logger.info(f"Cleanup Resources with Patterns: {RESOURCE_CLEANUP}")
+    logger.info(f"Generated Cleaned up File: {input_file}")
+
+
+def cleanup_tf_plan_file(input_tf_file):
+
+    # Process the Global defaults vaules
+    level1_cleanup_file = remove_global_lines(input_tf_file, RESOURCE_CLEANUP["global"])
+
+    # Process resource Specific Blocks
+    process_terraform_plan(level1_cleanup_file)
+    remove_multiline(input_tf_file, RESOURCE_CLEANUP["multiline_pattern"])
